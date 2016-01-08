cad1f902c7f3729b01f19db67c63d0344ba34ecd
Spotlight0xff <spotlight0xff@gmail.com>
2016-01-08 15:33:09 +0100

    added simple colours header

---
+++ code/colours.h
@@ -0,0 +1,7 @@
+#define WHITE  "\e[1;00m"
+#define RED    "\e[1;31m"
+#define GREEN  "\e[1;32m"
+#define YELLOW "\e[1;33m"
+#define BLUE   "\e[1;34m"
+#define PURPLE "\e[1;35m"
+#define CYAN   "\e[1;36m"
